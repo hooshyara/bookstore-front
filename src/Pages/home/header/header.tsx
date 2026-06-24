c953e0ff5da17ffe319e172c432d466647ae9728
hooshyara <arianhooshyar9@gmail.com>
2026-06-24 23:12:57 +0330

    fit:add Home page header

---
+++ src/Pages/home/header/header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./header.module.css";
+
+export default function Header() {
+  return <>
+    <img className={`${styles.heroImage}`} src="src/assets/home/Hero 2.png" alt="" />
+  </>;
+}
